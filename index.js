2ac29eb28b43316ed6bb016e40bc858d23e2421a
Moises Miguel <moimigueldev@gmail.com>
2021-09-21 16:16:43 -0500

    changed the shortcuts

--- index.js
+++ index.js
@@ -17,30 +17,36 @@ gk.start();
 // emitted events by process
 gk.on('press', (key) => {
   // OPENS TIMED WINDOW
-  if (key.data === '<Tab>') {
+  if (key.data === '`') {
     open = true;
     timer.stop();
+
     timer.start(500);
   }
 
-  // FOCUSES ON RIGHT MONITOR
-  if (open && key.data === '<Right>') {
+  // FOCUSES ON RIGHT MONITORclear
+
+  if (open && key.data === '3') {
+    console.log('Shifted focus to right monitor');
     screenRobot.focusRightMonitor();
     open = false;
   }
   // FOCUSES ON MAIN MONITOR
-  if ((open && key.data === '<Down>') || key.data === '<Enter>') {
+  if (open && key.data === '2') {
+    console.log('Shifted focus to main monitor');
     screenRobot.focusMainMonitor();
     open = false;
   }
   // FOCUSES ON LEFT MONITOR
-  if (open && key.data === '<Left>') {
+  if (open && key.data === '1') {
+    console.log('Shifted focus to left monitor');
     screenRobot.focusLeftMonitor();
     open = false;
   }
 });
 
 // process error
+
 gk.on('error', (error) => {
   console.error(error);
 });
